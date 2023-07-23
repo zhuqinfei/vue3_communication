a443e5ad26a6d4e28d252cfa2f0d0f6ebd413d6a
zhuqinfei <1586859762@qq.com>
2023-07-24 00:25:34 +0800

    vue3组件的通讯方式

---
+++ src/views/09_slot/SlotTest.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <h1>slot</h1>
+    <Test1 :todos="todos">
+      <template v-slot="{ $row, $index }">
+        <p :style="{ color: $row.done ? 'green' : 'red' }">
+          {{ $row.title }}--{{ $index }}
+        </p>
+      </template>
+    </Test1>
+  </div>
+</template>
+<script setup lang="ts">
+import Test from "./Test.vue";
+import Test1 from "./Test1.vue";
+//插槽:默认插槽、具名插槽、作用域插槽
+//作用域插槽:就是可以传递数据的插槽,子组件可以讲数据回传给父组件,父组件可以决定这些回传的
+//数据是以何种结构或者外观在子组件内部去展示！！！
+import { ref } from "vue";
+//todos数据
+let todos = ref([
+  { id: 1, title: "吃饭", done: true },
+  { id: 2, title: "睡觉", done: false },
+  { id: 3, title: "打豆豆", done: true },
+  { id: 4, title: "打游戏", done: false },
+]);
+</script>
+
+<style scoped>
+</style>
